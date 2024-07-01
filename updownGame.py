3a3fd6c2e6506e056c1611888f0e7a4ace881896
Dayoung <da4260@daum.net>
2024-07-01 17:53:19 +0900

    Update updownGame.py

--- updownGame.py
+++ updownGame.py
@@ -1,19 +1,13 @@
 import random
 
-# random_number = random.randint(1, 100)
-
 num = int(input("숫자를 입력하세요: "))
+
 
 def updown(num):
     random_number = random.randint(1, 100)
-
+    count = 0
     while True:
-        # num = int(input("숫자를 입력하세요: "))
-
-        # count += 1
-
         if 0 < num <= 100:
-            count = 0
             if random_number > num:
                 count += 1
                 print("업")
@@ -27,31 +21,20 @@ def updown(num):
                 print(f"시도한 횟수: {count}")
                 count = 0
                 break
-                # retry = input("다시 하시겠습니까? (y/n): ")
-                # if retry == 'y':
-                #     # random_number = random.randint(1, 100)
-                #     updown(num)
-                #     count = 0
-                # elif retry == 'n':
-                #     print("게임을 종료합니다")
-                #     break
-                # else:
-                #     print("y 또는 n을 입력하세요")
-                #     retry = input("다시 하시겠습니까? (y/n): ")
         else:
             print("유효한 범위 내의 숫자를 입력하세요")
             num = int(input("숫자를 입력하세요: "))
 
 updown(num)
 retry = input("다시 하시겠습니까? (y/n): ")
-while retry != 'n':
+while True:
     if retry == 'y':
-        # random_number = random.randint(1, 100)
         updown(num)
         retry = input("다시 하시겠습니까? (y/n): ")
         count = 0
     elif retry == 'n':
         print("게임을 종료합니다")
+        break
     else:
         print("y 또는 n을 입력하세요")
         retry = input("다시 하시겠습니까? (y/n): ")
